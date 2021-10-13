087038a10130cda52419bbba941857c6ae4bc12b
Carlos Lopez Z <krlosaren@gmail.com>
2021-10-13 16:33:07 -0300

    [rebuild] 2

--- pages/_document.js
+++ pages/_document.js
@@ -11,14 +11,22 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel='preconnect' href='https://fonts.googleapis.com' />
-          <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
+          <link
+            rel='preconnect'
+            href='https://fonts.gstatic.com'
+            crossOrigin='true'
+          />
           <link
             href='https://fonts.googleapis.com/css2?family=Kanit:wght@500;600;800&family=Noto+Sans+KR:wght@400;500;700;900&display=swap'
             rel='stylesheet'
           />
 
           <link rel='preconnect' href='https://fonts.googleapis.com' />
-          <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
+          <link
+            rel='preconnect'
+            href='https://fonts.gstatic.com'
+            crossOrigin='true'
+          />
           <link
             href='https://fonts.googleapis.com/css2?family=Kanit:wght@500;600;800&family=Montserrat:wght@200;300;400;500;600;700;800;900&family=Noto+Sans+KR:wght@400;500;700;900&display=swap'
             rel='stylesheet'
